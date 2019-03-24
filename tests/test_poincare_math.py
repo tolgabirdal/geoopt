371d97a589de20b37c09168ea938386b38d76996
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2019-03-24 17:35:23 +0300

    test_mobius_negative_addition

--- tests/test_poincare_math.py
+++ tests/test_poincare_math.py
@@ -94,14 +94,14 @@ def test_mobius_addition_negative_cancellation(a, r):
     np.testing.assert_allclose(res, torch.zeros_like(res), **tolerance[r.dtype])
 
 
-def test_mobius_negative_addition(a, b, c):
-    res = poincare.math.mobius_add(-b, -a, c=c)
-    res1 = -poincare.math.mobius_add(b, a, c=c)
+def test_mobius_negative_addition(a, b, r):
+    res = poincare.math.mobius_add(-b, -a, r=r)
+    res1 = -poincare.math.mobius_add(b, a, r=r)
     tolerance = {
         torch.float32: dict(atol=1e-7, rtol=1e-6),
         torch.float64: dict(atol=1e-10),
     }
-    np.testing.assert_allclose(res, res1, **tolerance[c.dtype])
+    np.testing.assert_allclose(res, res1, **tolerance[r.dtype])
 
 
 @pytest.mark.parametrize("n", list(range(5)))
